20bb3f1723024ff839446e1b3fdcd71e0819b75e
Fanghe <fanghe@bytedance.com>
2021-09-26 18:06:13 +0800

    init

---
+++ Carryouhome/Shared/CarryouhomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarryouhomeApp.swift
+//  Shared
+//
+//  Created by fanghe on 2021/9/26.
+//
+
+import SwiftUI
+
+@main
+struct CarryouhomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
